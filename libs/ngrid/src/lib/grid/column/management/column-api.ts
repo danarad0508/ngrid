6dbd10a412b1c1b3c29e6e107fbd85d2edfadcff
Dan Arad <dan.arad@sharegain.com>
2025-01-20 11:07:50 +0200

    Migrated to nx 15

--- libs/ngrid/src/lib/grid/column/management/column-api.ts
+++ libs/ngrid/src/lib/grid/column/management/column-api.ts
@@ -37,7 +37,7 @@ export class ColumnApi<T> {
         .pipe(
           ON_RESIZE_ROW,
           // We might get a null sizeInfo when a new column is added - see syncColumnGroupsSize()
-          map( e => this.grid.columnApi.visibleColumns.reduce( (p, c) => p + c.sizeInfo?.width ?? 0, 0 ) ),
+          map( e => this.grid.columnApi.visibleColumns.reduce( (p, c) => p + (c.sizeInfo?.width ?? 0), 0 ) ),
         );
     }
     return this._totalColumnWidthChange;
